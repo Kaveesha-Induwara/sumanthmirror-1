5ced6a5a64bf9d846e59d05e3bd4a51429ff6069
jikkubot <66976170+jikkubot@users.noreply.github.com>
2020-11-23 08:26:03 +0400

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -1,6 +1,8 @@
+from telegram import InlineKeyboardMarkup
 from telegram.message import Message
 from telegram.update import Update
 import time
+import psutil
 from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, bot, \
     status_reply_dict, status_reply_dict_lock
 from bot.helper.ext_utils.bot_utils import get_readable_message
@@ -17,10 +19,19 @@ def sendMessage(text: str, bot, update: Update):
         LOGGER.error(str(e))
 
 
-def editMessage(text: str, message: Message):
+def sendMarkup(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
+    try:
+        return bot.send_message(update.message.chat_id,
+                             reply_to_message_id=update.message.message_id,
+                             text=text, reply_markup=reply_markup, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
+
+
+def editMessage(text: str, message: Message, reply_markup=None):
     try:
         bot.edit_message_text(text=text, message_id=message.message_id,
-                              chat_id=message.chat.id,
+                              chat_id=message.chat.id,reply_markup=reply_markup,
                               parse_mode='HTMl')
     except Exception as e:
         LOGGER.error(str(e))
@@ -64,6 +75,9 @@ def delete_all_messages():
 
 def update_all_messages():
     msg = get_readable_message()
+    msg += f"<b>CPU:</b> {psutil.cpu_percent()}%" \
+           f" <b>DISK:</b> {psutil.disk_usage('/').percent}%" \
+           f" <b>RAM:</b> {psutil.virtual_memory().percent}%"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -76,6 +90,9 @@ def update_all_messages():
 
 def sendStatusMessage(msg, bot):
     progress = get_readable_message()
+    progress += f"<b>🔹CPU:</b> {psutil.cpu_percent()}%" \
+           f" <b>🔹DISK:</b> {psutil.disk_usage('/').percent}%" \
+           f" <b>🔹RAM:</b> {psutil.virtual_memory().percent}%"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
